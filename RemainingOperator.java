a559dbb774bafc078642980fceec425beaefeafe
PraveenKumar S <praveenkrsslm69@gmail.com>
2026-04-09 17:01:35 +0530

    Add files via upload

---
+++ RemainingOperator.java
@@ -0,0 +1,124 @@
+package Java_intro;
+
+public class RemainingOperator {
+
+	public static void main(String[] args) {
+	// unary operator
+		int a = 10;
+//		System.out.println(a);
+//		a++;
+//		System.out.println(a);
+//		++a;
+//		System.out.println(a);
+//		
+//		System.out.println(a);
+//		a--;
+//		System.out.println(a);
+//		--a;
+//		System.out.println(a);
+		//inc
+//		System.out.println(++a);//11
+//		System.out.println(a++);
+//		System.out.println(a);
+//		//dec
+//		System.out.println(--a);//9
+//		System.out.println(a--);
+//		System.out.println(a);
+		
+		
+	//ternary opertor
+		/*
+		 * Syntax of ternary
+		 * 
+		 *datatype variable = condition?expression1:exprestion2;
+		 */
+		
+		
+//		int b = 2;
+//		String v = b==0 ? "B is Zero":"B is Not a Zero";
+//		System.out.println(v);
+		
+		
+		
+		
+//		Conditional Statement -> if Statement
+		//1
+//		int b = 3;
+//		if(b==0) {
+//			System.out.println("B is zero!!");
+//		}
+//		else {
+//			System.out.println("B is not zero");
+//		}
+		
+		//2.odd or even
+		
+//		int n = 10;
+//		if(n%2 == 0) {
+//			System.out.println(n+" is even number");
+//		}
+//		else{
+//			System.out.println(n+" is odd number");
+//		}
+		
+		//3.postive/negative
+		
+//		int n = -10;
+//		if(n>0) {
+//			System.out.println(n+" is positive number");
+//		}
+//		else{
+//			System.out.println(n+" is Negative number");
+//		}		
+		
+		
+		//else if statement
+		
+		int day=20;
+		
+		if(day>=1 && day<=5) {
+			System.out.println("Bill Amount:"+(day*3));
+		}
+		else if(day>=6 && day<=10) {
+			System.out.println("Bill Amount:"+(day*5));
+		}
+		else if(day>=11 && day<=15) {
+			System.out.println("Bill Amount:"+(day*10));
+		}
+		else {
+			System.out.println("Bill Amount:"+(day*15));
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
