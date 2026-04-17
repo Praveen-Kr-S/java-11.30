580dc3e7d6978d4d39742b50c92c92f0f0f0dc14
PraveenKumar S <praveenkrsslm69@gmail.com>
2026-04-17 13:39:24 +0530

    Add files via upload

---
+++ UserInputs.java
@@ -0,0 +1,143 @@
+package flow_of_control;
+
+import java.util.Scanner;
+
+public class UserInputs {
+	public static void main(String[] args) {
+		
+//		Scanner scan = new Scanner(System.in);
+//		System.out.println("Enter Your Name :");
+//		String name = scan.nextLine();
+//		System.out.println("Name:"+name);
+//		
+//		System.out.println("Enter Your Age :");
+//		byte age = scan.nextByte();
+//		System.out.println("Age :"+age);
+//		
+//		System.out.println("Enter Your CGPA :");
+//		float CGPA = scan.nextFloat();
+//		System.out.println("CGPA :"+CGPA);
+		
+		
+//		loops/iterative/repetation control
+		
+//		for loop inc
+		
+//		for(int i=5;i<=10;i++) {
+//			System.out.println(i+" Praveen");
+//		}
+		
+//		for(int i=0;i<=50;i+=5) {
+//			System.out.println(i+" Praveen");
+//		}
+		
+//		for loop dec
+//		for(int i=10; i>=6; i--) {
+//			
+//			System.out.println(i);
+//		}
+		
+//		for(int i=10; i>=6; i-=2) {
+//				System.out.println(i);
+//			}
+		
+		
+//		infinity for loop
+//		for(int i=10; true; i--) {
+//			System.out.println(i);
+//		}
+		
+//		while loop - inc
+		
+//		int i = 5;
+//		while(i<=10) {
+//			System.out.println(i);
+//			i+=2; //10+1 = 11
+//		}
+		
+		
+//while loop - dec
+		
+//		int i = 5;
+//		while(i>0) {
+//			System.out.println(i);
+//			i--; 
+//		}		
+		
+		
+//		infinity while loops
+		
+//		int i = 5;
+//		while(i>0) {
+//			System.out.println(i);
+//			i++; 
+//		}
+		
+		
+		
+//		find factorial 
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter Value : ");
+		int f = scan.nextInt();
+		
+		int v = 1;
+		
+		for(int i = 1; i <= f;i++) {
+			v*=i;
+		}
+		
+		System.out.println("Factorial Value :"+v);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
