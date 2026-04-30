09fef17c8b248b0e464fe2328df0352def0c2eae
PraveenKumar S <praveenkrsslm69@gmail.com>
2026-04-30 17:03:28 +0530

    Update SdArray.java

--- SdArray.java
+++ SdArray.java
@@ -58,111 +58,102 @@ public class SdArray {
 //		multi-dimensional array
 //		2D array (rows and column)
 		
-		int arr[][] = new int[2][3];
-		
-		arr[0][0]=10;
-		arr[0][1]=20;
-		arr[0][2]=30;
-		arr[1][0]=40;
-		arr[1][1]=50;
-		arr[1][2]=20;
-//		System.out.println(arr[1][1]);
-//		for(int i = 0;i<2;i++) {
-//			for(int j = 0;j<3;j++) {
-//				System.out.print(arr[i][j]+" ");
-//			}
-//			System.out.println();
-//		}
-		
-		
-//		int arr1[][] = {{1,2,3},{4,5,6},{6,7,8}};
+//		int arr[][] = new int[2][3];
 //		
-//		for(int i = 0;i<3;i++) {
-//			for(int j = 0;j<3;j++) {
-//				System.out.print(arr1[i][j]+" ");
+//		arr[0][0]=10;
+//		arr[0][1]=20;
+//		arr[0][2]=30;
+//		arr[1][0]=40;
+//		arr[1][1]=50;
+//		arr[1][2]=20;
+////		System.out.println(arr[1][1]);
+////		for(int i = 0;i<2;i++) {
+////			for(int j = 0;j<3;j++) {
+////				System.out.print(arr[i][j]+" ");
+////			}
+////			System.out.println();
+////		}
+//		
+//		
+////		int arr1[][] = {{1,2,3},{4,5,6},{6,7,8}};
+////		
+////		for(int i = 0;i<3;i++) {
+////			for(int j = 0;j<3;j++) {
+////				System.out.print(arr1[i][j]+" ");
+////			}
+////			System.out.println();
+////		}
+//		
+//		Scanner scan = new Scanner(System.in);
+//		System.out.print("Enter the Array No of Rows :");
+//		int row = scan.nextInt();
+//		System.out.print("Enter the Array No of Column :");
+//		int col = scan.nextInt();
+//		int arr2[][] = new int[row][col];
+//		
+//		for(int i = 0; i<row;i++) {
+//			for(int j = 0; j<col;j++) {
+//				System.out.println("row "+i+" column "+j+" value :");
+//				arr2[i][j] = scan.nextInt();
 //			}
-//			System.out.println();
 //		}
-		
-		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter the Array No of Rows :");
-		int row = scan.nextInt();
-		System.out.print("Enter the Array No of Column :");
-		int col = scan.nextInt();
-		int arr2[][] = new int[row][col];
-		
-		for(int i = 0; i<row;i++) {
-			for(int j = 0; j<col;j++) {
-				System.out.println("row "+i+" column "+j+" value :");
-				arr2[i][j] = scan.nextInt();
-			}
-		}
+//		
+//		
+//		for(int i = 0;i<row;i++) {
+//		for(int j = 0;j<col;j++) {
+//			System.out.print(arr2[i][j]+" ");
+//		}
+//		System.out.println();
+//	}
+//		
 		
 		
-		for(int i = 0;i<row;i++) {
-		for(int j = 0;j<col;j++) {
-			System.out.print(arr2[i][j]+" ");
-		}
-		System.out.println();
-	}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+//		java array task
+//		1.sum of the array
+//		int a[] = {10,20,30,40,50};
+//		int v=0;
+//		for(int i=0;i<a.length;i++) {
+//			v+=a[i];
+//		}
+//		System.out.println(v);
 
+				
+//	 2.to find max value of array	
+//		int a1[] = {30,55,12,56,23,87,34};
+//		int v1 = a1[0];
+//		for(int i=0;i<a1.length;i++) {
+//			if (a1[i]<v1) {
+//			//   34  > 87
+//				v1 = a1[i]; //87
+//			}
+//		}
+//		
+//		System.out.println("Array Max Vale : "+v1);
+//		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+//		3. to find given value of array	
+//			Scanner scan = new Scanner(System.in);
+//			System.out.println("Enter the Finding Value : ");
+//			int f = scan.nextInt();
+//			int a1[] = {30,55,12,56,23,87,34};
+//			boolean check = false;
+//			for(int i=0;i<a1.length;i++) {
+//				if (a1[i] == f) {
+//					check = true;
+//					break;
+//				}
+//			}
+//			
+//			if(check == true) {
+//				System.out.println(f+" Value is Available in Array");
+//			}else {
+//				System.out.println(f+" Value is Not Available in Array");
+//			}
+			
+			
+			
+			
+			
 		
 
 	}
