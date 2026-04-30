750c200de230b68cc961298ef1ddfa3c8c4c3931
PraveenKumar S <praveenkrsslm69@gmail.com>
2026-04-30 17:05:50 +0530

    Add files via upload

---
+++ Demo_strins.java
@@ -0,0 +1,102 @@
+package Strings_learn;
+
+public class Demo_strins {
+
+	public static void main(String[] args) {
+		
+		
+//		Strings litral method
+		
+		
+//		String s = "Praveen";
+		//index --> 0123456
+		//length -> 1234567
+		
+//		System.out.println(s);
+//		System.out.println(s.charAt(3));
+//		
+//		String s1 = "Praveen";
+//		
+//		//new keyword
+//		String s2 = new String("Praveen1");
+//		System.out.println(s2);
+//		System.out.println(s == s1);
+//		System.out.println(s1 == s2);
+//		
+//		System.out.println(s1.equals(s2));
+//		
+////		String Build in functions
+////		stringData.Buildinfunctions or variable.Buildinfunctions
+//		System.out.println(s1.toUpperCase());
+//		System.out.println(s1.toLowerCase());
+//		System.out.println(s1.codePointAt(2));
+//		System.out.println(s1.codePointBefore(3));
+//		System.out.println(s1.compareTo(s2));
+//		System.out.println("JAVA".compareToIgnoreCase("java"));
+//		System.out.println(s.concat(" Kumar"));
+//		System.out.println(s.contains("P"));
+//		System.out.println(s.endsWith("en"));
+//		System.out.println(s.startsWith("Prv"));
+//		System.out.println(s.equalsIgnoreCase("praveen"));
+//		System.out.println(s.hashCode());
+//		System.out.println(s.indexOf("v"));
+//		System.out.println(s.length());
+//		System.out.println(s.replace('n', 'N'));
+//		System.out.println(s);
+		
+		
+
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+
+	}
+
+}
