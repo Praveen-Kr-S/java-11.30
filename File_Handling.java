3684763b374067a18d6d57ed8ea9316ed06fa789
PraveenKumar S <praveenkrsslm69@gmail.com>
2026-05-22 13:42:56 +0530

    Add files via upload

---
+++ File_Handling.java
@@ -0,0 +1,127 @@
+package Java_intro;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class File_Handling {
+
+	public static void main(String[] args) throws IOException {
+		
+//		 File Handling
+//		
+//		File f = new File("C:\\Users\\Live Wire\\Desktop\\Vasath.txt");
+//		f.createNewFile();
+//		System.out.println("File Created");
+		
+//		Write file
+//		FileWriter w = new FileWriter(new File("C:\\Users\\Live Wire\\Desktop\\Dhanush.txt"));
+//		
+//		w.write("Hello Praveen, Bye Praveen...");
+//		w.flush();
+//		w.close();
+//		System.out.println("Data Writted");
+		
+		
+//		Append file
+		
+//		FileWriter w = new FileWriter(new File("C:\\Users\\Live Wire\\Desktop\\Dhanush.txt"));
+//		w.append("\nBye Praveen, I am Vasath");
+//		w.flush();
+//		w.close();
+//		System.out.println("Data Writted");		
+//		
+		
+		//Read the file
+//		Scanner scan = new Scanner(new File("C:\\Users\\Live Wire\\Desktop\\Dhanush.txt"));
+		
+//		System.out.println(scan.next());
+//		System.out.println(scan.next());
+		
+//		while(scan.hasNext()) {
+//			System.out.println(scan.next());
+//		}
+//		
+		
+		// delete the file
+		
+//		File f = new File("C:\\Users\\Live Wire\\Desktop\\Dhanush.txt");
+//		f.delete();
+//		System.out.println("File Deleted");
+		
+		
+		
+		
+//		Exception handling
+		
+//		System.out.println("Hello");
+//		int a = 10;
+//		int b = 1 ;
+//		System.out.println(a/b);\
+		
+		
+//		try {
+//			int a = 10;
+//			int b = 5;
+//			int c = 2;
+////			System.out.println("Add Value : "+(a+b+c));
+////			System.out.println("divide Value : "+(b/c));
+//		}
+//		catch (Exception e){
+////			System.out.println(e);
+//			System.out.println("Can't be divide in zero");
+//		}
+		
+		//2
+//		try {
+//			int ar[] = {10,20,30,40,50};
+//			
+//			
+////			System.out.println(ar[0]);
+//			System.out.println(ar[8]);
+// 		}
+//		catch(Exception e) {
+//			System.out.println(e);
+//		}
+//		finally{
+//			System.out.println("Code Executed");
+//		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
